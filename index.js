2d26459eede4eb163a42d279af916e70df85ba10
Pesky9 <ypesky0@gmail.com>
2024-08-15 20:06:38 +0530

    Changed index.js

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-const http = require("http");
 const app = require("./app");
 const { ConnectDB } = require("./src/db/db");
 
-ConnectDB()
-  .then(() => {
-    http.createServer(app).listen(200, (req, res) => {
-      console.log("Server started at port 200");
-    });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+ConnectDB();
+
+module.exports = app;
