99e2cb665afbb9a1db461aa306883d2f4541dc91
Pesky9 <ypesky0@gmail.com>
2024-08-15 19:35:48 +0530

    Updated CORS

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const cors = require("cors");
 const router = require("./src/routes/user.routes");
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://crud-app-psi-lime.vercel.app"],
+    methods: ["GET", "POST", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/", router);
 
