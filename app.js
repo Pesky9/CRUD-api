c899bf0d6da7fadf456d96ede1f1b79d360ccbe4
Pesky9 <ypesky0@gmail.com>
2024-08-15 19:40:07 +0530

    Added DB Connection in App.js

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const router = require("./src/routes/user.routes");
 const app = express();
+const { ConnectDB } = require("./src/db/db");
+
+ConnectDB();
 
 app.use(
   cors({
